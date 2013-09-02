4c2bb2bc8c5e8f3b5983642ff0e8b759093f3259
Joel Kim <joel@wtx.com.au>
2013-09-02 00:20:12 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifegame::Application.config.secret_key_base = '04f01f95f6655d5348bc0db6443389d6658bd25fc9d230b2983b4933321c5ad280ef3cd853c58d1d92f0363efc0e07f03aea6f696c1d22c228408dd852f3a14b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lifegame::Application.config.session_store :cookie_store, key: '_lifegame_session'
